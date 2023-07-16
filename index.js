7edba664fe2c27b87e8e8750980d06a52326b96c
Theodore Smith <tjs7321@gmail.com>
2023-07-16 14:17:48 -0400

    dnoen

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+      return renderBooks(data)
+    });
 }
 
 function renderBooks(books) {
